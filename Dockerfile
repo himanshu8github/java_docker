ff6c4e1a613dd7ca39b3809728088a7176aeb85c
Ubuntu <ubuntu@ip-172-31-5-85.ap-south-1.compute.internal>
2026-09-07 09:02:40 +0000

    update docker file

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM eclipse-temurin:17-jdk-alpine
 
+
 WORKDIR /app
 
-COPY src/Main.java .
+COPY  src/Main.java /app/Main.java
 
 RUN javac Main.java
 
-CMD ["java", "Main"]
+CMD ["java", "Main"] 
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+	System.out.println("hello from docker");
         System.out.println("Current Date and Time: " + now.format(formatter));
     }
 }
